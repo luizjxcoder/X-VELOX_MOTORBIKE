a9a79bee20f0c4e67cbd4f784169706b98d3c594
luizjxcoder <luiz.jun19@gmail.com>
2022-01-15 20:55:53 +0044

    script

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const links = document.querySelectorAll('header-menu a');
+
+function ativarLink(link){
+    const url = location.href;
+    const href = link.href;
+    if (url.includes(href)) {
+        link.classList.add("ativo");
+    }
+}
+links.forEach(ativarLink);
