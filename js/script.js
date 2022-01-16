1e50f743da19847279ae89140d8a9cb82e7670d9
luizjxcoder <luiz.jun19@gmail.com>
2022-01-16 08:20:03 +0044

    script perguntas

--- js/script.js
+++ js/script.js
@@ -26,12 +26,38 @@ parametros.forEach(ativarProduto);
 
 const  perguntas = document.querySelectorAll(".perguntas button");
 
-function ativarPerguntas(event){
-    const pergunta = event.currentTaget;
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
 }
 
 function eventosPerguntas(pergunta){
-    pergunta.addEventListener('click', ativarPergunta);
+    pergunta.addEventListener("click", ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas);
+
+
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
 }
 
-perguntas.forEach(eventosPerguntas);
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
